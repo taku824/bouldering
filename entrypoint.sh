31946da7c13fe6a692d6c40175b0844406c4045a
astax <augusttaku824@gmail.com>
2025-09-25 02:12:24 +0900

    Configure health check and optimize database operations

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-# Create database if it doesn't exist and run migrations
-bin/rails db:create db:migrate
+# Create database if it doesn't exist and run migrations in background
+bin/rails db:create db:migrate &
 
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
+# Start the Rails server
 exec "$@"
